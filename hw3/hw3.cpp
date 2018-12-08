f6ea3e0cf5e82e6324b4392fd6bb89fc24498f10
mlkmhmtbyk <biyikm16@itu.edu.tr>
2018-12-09 01:28:02 +0300

    sabri

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -1,11 +1,11 @@
-// hw3.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	asdasd
+
     return 0;
 }
 
